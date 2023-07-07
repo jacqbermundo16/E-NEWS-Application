f0b861fcbe6f1498739dd9ebd47e8a6a5ab7838e
Primo <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2023-07-07 20:51:11 +0800

    Added a functional side navigation bar, an about fragment.

--- app/src/main/java/codeblock/app/e_news/MainActivity.kt
+++ app/src/main/java/codeblock/app/e_news/MainActivity.kt
@@ -1,32 +1,80 @@
 package codeblock.app.e_news
 
-import android.content.AbstractThreadedSyncAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import codeblock.app.e_news.databinding.ActivityMainBinding
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
-import codeblock.app.e_news.newsfeed.NewsFeedAdapter
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var navController: NavController
-    private lateinit var newsAdapter: NewsFeedAdapter
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-        setContentView(R.layout.fragment_favorites)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
         navController = navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navbar)
         setupWithNavController(bottomNavigationView, navController)
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.side_navbar)
+
+        val toolbar = findViewById<Toolbar>(R.id.side_toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_sidebarView)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            replaceFragment(News())
+            navigationView.setCheckedItem(R.id.news)
+        }
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.news -> replaceFragment(News())
+            R.id.donate -> replaceFragment(Donate())
+            R.id.favorites -> replaceFragment(Favorites())
+            R.id.profile -> replaceFragment(Profile())
+            R.id.aboutUs -> replaceFragment(About())
+            R.id.logout -> Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, fragment)
+        transaction.commit()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
 
 }
--- app/src/main/java/codeblock/app/e_news/newsfeed/NewsItem.kt
+++
@@ -1,5 +0,0 @@
-package codeblock.app.e_news.newsfeed
-
-class NewsItem(val title: String, val description: String, val imageUrl: String) {
-    // Additional properties and methods
-}
