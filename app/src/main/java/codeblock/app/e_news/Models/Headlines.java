99382dc6f4f40877ceae736d4a8eb7142a455389
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-07-16 18:24:20 +0800

    Creating Models (Headlines)

---
+++ app/src/main/java/codeblock/app/e_news/Models/Headlines.java
@@ -0,0 +1,21 @@
+package codeblock.app.e_news.Models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Headlines {
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    @SerializedName("totalResults")
+    @Expose
+    private String totalResults;
+
+    @SerializedName("articles")
+    @Expose
+    private List<Articles> articles;
+}
