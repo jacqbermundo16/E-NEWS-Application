9207646fbca88a2a56b562730d4276fe3af8eead
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-07-16 18:28:59 +0800

    Generating getters and setters (Headlines)

--- app/src/main/java/codeblock/app/e_news/Models/Headlines.java
+++ app/src/main/java/codeblock/app/e_news/Models/Headlines.java
@@ -18,4 +18,28 @@ public class Headlines {
     @SerializedName("articles")
     @Expose
     private List<Articles> articles;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(String totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Articles> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Articles> articles) {
+        this.articles = articles;
+    }
 }
