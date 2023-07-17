7e394f905041702a36458044c417a367edc92439
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-07-17 15:16:24 +0800

    updating the main activity to see if the api is successfully connected

--- app/src/main/java/codeblock/app/e_news/MainActivity.kt
+++ app/src/main/java/codeblock/app/e_news/MainActivity.kt
@@ -13,17 +13,38 @@ import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import androidx.recyclerview.widget.RecyclerView
+import codeblock.app.e_news.Adapter
+import codeblock.app.e_news.Models.Articles
+import codeblock.app.e_news.Models.Headlines
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.Locale
+
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var recyclerView: RecyclerView
+    private val API_KEY = "e047631ef2msh77fa3a93a675193p17e232jsn5aa342e77daf"
+    private lateinit var adapter: Adapter
+    private val articles: ArrayList<Articles> = ArrayList()
+
 
     private lateinit var navController: NavController
     private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.topNews);
+        recyclerView.layoutManager = LinearLayoutManager(this);
+        val country = getCountry();
+        retrieveJson(country,API_KEY);
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
@@ -76,6 +97,30 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         } else {
             onBackPressedDispatcher.onBackPressed()
         }
+    }
+
+    private fun retrieveJson(country: String, apiKey: String) {
+        val call: Call<Headlines> =
+            ApiClient.getInstance().api.getHeadlines(country, apiKey)
+        call.enqueue(object : Callback<Headlines> {
+            override fun onResponse(call: Call<Headlines>, response: Response<Headlines>) {
+                if (response.isSuccessful && response.body()?.articles != null) {
+                    articles.clear()
+                    articles.addAll(response.body()?.articles!!)
+                    adapter = Adapter(this@MainActivity, articles)
+                    recyclerView.adapter = adapter
+                }
+            }
+
+            override fun onFailure(call: Call<Headlines>, t: Throwable) {
+                Toast.makeText(this@MainActivity, t.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun getCountry(): String {
+        val locale: Locale = Locale.getDefault()
+        return locale.country.toLowerCase()
     }
 
 }
