60015873c4d1139df34d1641370a172e67bc386e
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-07-23 18:36:33 +0800

    updated files, generated notification function and attach it with its layout

--- app/src/main/java/codeblock/app/e_news/MyFirebaseMessagingService.kt
+++ app/src/main/java/codeblock/app/e_news/MyFirebaseMessagingService.kt
@@ -1,10 +1,45 @@
 package codeblock.app.e_news
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
+const val channelId = "notification_channel"
+const val channelName = "codeblock.app.e_news"
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     //generate the notification
     //attach the notification created with the custom layout
     //show the notification
+
+    fun getRemoteView(title: String, message: String): RemoteViews{
+        val remoteView = RemoteViews("codeblock.app.e_news", R.layout.notification)
+
+        remoteView.setTextViewText(R.id.title,title)
+        remoteView.setTextViewText(R.id.message,message)
+        remoteView.setImageViewResource(R.id.app_logo, R.drawable.logo_transparent)
+
+        return remoteView
+    }
+    fun generateNotification(title: String, message: String){
+
+        val intent = Intent(this,MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE)
+
+        var builder: NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.logo_transparent)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000,1000,1000,1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+
+        builder = builder.setContent(getRemoteView(title,message))
+
+
+    }
 }
