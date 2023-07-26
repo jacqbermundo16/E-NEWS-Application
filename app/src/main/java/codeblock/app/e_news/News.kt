0d36cf6af4443c18223883889e46960b47005a97
DeniseIra02 <irajubilo@gmail.com>
2023-07-26 16:42:54 +0800

    Update News.kt
    Functionality of search button added

--- app/src/main/java/codeblock/app/e_news/News.kt
+++ app/src/main/java/codeblock/app/e_news/News.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -31,13 +33,28 @@ class News : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = view.findViewById<RecyclerView>(R.id.topNews)
+        val searchText = view.findViewById<EditText>(R.id.searchText)
+        val searchBtn = view.findViewById<Button>(R.id.searchBtn)
+
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
 
         // Initialize the adapter and set it to the RecyclerView
         adapter = Adapter(requireContext(), articles)
         recyclerView.adapter = adapter
 
+        // Fetch initial data on fragment launch
         retrieveJson(apiKey)
+
+        // Set click listener for the search button
+        searchBtn.setOnClickListener {
+            val searchQuery = searchText.text.toString()
+            if (searchQuery.isNotBlank()) {
+                searchArticles(searchQuery)
+            } else {
+                // If the search query is empty, show a toast message
+                Toast.makeText(requireContext(), "Enter a search query", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun retrieveJson(apiKey: String) {
@@ -57,13 +74,35 @@ class News : Fragment() {
                     val articleList = response.body()?.articles
                     Log.d("Jetty Boy", "API Response Articles size: ${articleList?.size}")
 
-                    // Log article titles and image URLs
-                    for (article in articleList!!) {
-                        Log.d("Articles", "Title: ${article.title}, Image URL: ${article.urlToImage}")
+                    articles.clear()
+                    articleList?.let {
+                        articles.addAll(it)
                     }
+                    adapter.notifyDataSetChanged()
+                } else {
+                    Log.d("Jetty Boy", "API call was not successful: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<Headlines?>, t: Throwable) {
+                Toast.makeText(requireContext(), t.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun searchArticles(query: String) {
+        Log.d("News", "searchArticles function called with query: $query")
+        val call: Call<Headlines?>? = ApiClient.getInstance().api.getNewsByQuery(query, apiKey)
+        call?.enqueue(object : Callback<Headlines?> {
+            override fun onResponse(call: Call<Headlines?>, response: Response<Headlines?>) {
+                if (response.isSuccessful && response.body()?.articles != null) {
+                    val articleList = response.body()?.articles
+                    Log.d("Jetty Boy", "API Response Articles size: ${articleList?.size}")
 
                     articles.clear()
-                    articles.addAll(articleList)
+                    articleList?.let {
+                        articles.addAll(it)
+                    }
                     adapter.notifyDataSetChanged()
                 } else {
                     Log.d("Jetty Boy", "API call was not successful: ${response.code()}")
