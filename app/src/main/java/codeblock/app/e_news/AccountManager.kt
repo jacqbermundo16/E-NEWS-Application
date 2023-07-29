e2f1d69cc4a1f5ec2b3feb85a9806ea44789682b
DeniseIra02 <irajubilo@gmail.com>
2023-07-29 21:19:30 +0800

    Deleting current user account is working successfully.

--- app/src/main/java/codeblock/app/e_news/AccountManager.kt
+++ app/src/main/java/codeblock/app/e_news/AccountManager.kt
@@ -1,59 +1,152 @@
 package codeblock.app.e_news
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.InputType
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountManager.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AccountManager : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var accountReference: DatabaseReference
+    private lateinit var input: EditText // Define the input variable at the class level
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        accountReference = FirebaseDatabase.getInstance().getReference("accounts")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account_manager, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountManager.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AccountManager().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnDelAcc = view.findViewById<Button>(R.id.btnDelAcc)
+        btnDelAcc.setOnClickListener {
+            showConfirmationDialog()
+        }
+    }
+
+    private fun showConfirmationDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Confirmation")
+        builder.setMessage("Are you sure you want to delete your account?")
+
+        builder.setPositiveButton("Yes") { _, _ ->
+            showPasswordConfirmationDialog()
+        }
+
+        builder.setNegativeButton("No") { _, _ ->
+            // User clicked "No", do nothing
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showPasswordConfirmationDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Confirm Password")
+        input = EditText(requireContext())
+        input.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+        builder.setView(input)
+
+        builder.setPositiveButton("Confirm") { _, _ ->
+            val password = input.text.toString()
+            if (password.isNotEmpty()) {
+                deleteAccount(password)
+            } else {
+                Toast.makeText(requireContext(), "Please enter your password.", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        builder.setNegativeButton("Cancel") { _, _ ->
+            // User clicked "Cancel", do nothing
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun deleteAccount(password: String) {
+        val currentUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
+        if (currentUser != null) {
+            // Re-authenticate the user to ensure the provided password is correct
+            val credentials = EmailAuthProvider.getCredential(currentUser.email!!, password)
+            currentUser.reauthenticate(credentials)
+                .addOnSuccessListener {
+                    // Re-authentication successful, proceed with deleting user data from Realtime Database
+                    deleteUserDataFromDatabase(currentUser.uid)
+                }
+                .addOnFailureListener { exception ->
+                    // Re-authentication failed, show an error message
+                    Toast.makeText(requireContext(), "Failed to verify your password. Please try again.", Toast.LENGTH_SHORT).show()
+                    Log.e("codeblock.app.e_news.AccountManager", "Error re-authenticating user: ${exception.message}")
+                }
+        } else {
+            // If the current user is null, show an error message
+            Toast.makeText(requireContext(), "Failed to delete account. User not found.", Toast.LENGTH_SHORT).show()
+            Log.e("codeblock.app.e_news.AccountManager", "Error deleting account: User is null.")
+        }
+    }
+
+    private fun deleteUserDataFromDatabase(userId: String) {
+        Log.d("codeblock.app.e_news.AccountManager", "User ID to delete: $userId")
+        val usersRef: DatabaseReference = FirebaseDatabase.getInstance().getReference("users")
+
+        usersRef.child(userId).removeValue()
+            .addOnSuccessListener {
+                // User data deleted successfully, now delete the user from Firebase Authentication
+                deleteFirebaseUser()
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(requireContext(), "Failed to delete account data. Please try again.", Toast.LENGTH_SHORT).show()
+                Log.e("codeblock.app.e_news.AccountManager", "Error deleting account data: ${exception.message}")
+            }
+    }
+
+    private fun deleteFirebaseUser() {
+        val currentUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
+        if (currentUser != null) {
+            currentUser.delete()
+                .addOnSuccessListener {
+                    // User deleted from Firebase Authentication
+                    navigateToSignUpActivity()
+                }
+                .addOnFailureListener { exception ->
+                    // User deletion failed, show an error message
+                    Toast.makeText(requireContext(), "Failed to delete account. Please try again.", Toast.LENGTH_SHORT).show()
+                    Log.e("codeblock.app.e_news.AccountManager", "Error deleting user from Firebase Authentication: ${exception.message}")
+                }
+        } else {
+            // If the current user is null, show an error message
+            Toast.makeText(requireContext(), "Failed to delete account. User not found.", Toast.LENGTH_SHORT).show()
+            Log.e("codeblock.app.e_news.AccountManager", "Error deleting user from Firebase Authentication: User is null.")
+        }
+    }
+
+    private fun navigateToSignUpActivity() {
+        val intent = Intent(requireContext(), LandingActivity::class.java)
+        startActivity(intent)
+        requireActivity().finish()
     }
 }
